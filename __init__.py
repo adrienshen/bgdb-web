c7c8851d08404d6e51848ffbf5b192d225fd37bb
adrienshen <adrienshen.dev@gmail.com>
2019-09-19 12:45:58 +0800

    Finish p2 of python mega tutorial

--- __init__.py
+++ __init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Z3HomuOuDX'
+app.config['DEV_EMAIL'] = 'adrienshen.dev@gmail.com'
+app.config['AMAZON_AFFILIATE_TOKEN'] = ''
 
 from app import routes
--- routes.py
+++ routes.py
@@ -1,6 +1,40 @@
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Board Games DB!"
+    user = {'username': 'BGDB user #5'}
+    main_display = []
+    specials = []
+    home_title = "Welcome to your guide in the world of board gaming"
+    announcement = "We just launched!"
+    board_game_cafes_near_by = []
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts, home_title=home_title)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
+
+
+@app.route('/board-games')
+def boardgame_list():
+    return 'board games list'
+
